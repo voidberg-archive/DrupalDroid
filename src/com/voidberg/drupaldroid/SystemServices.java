d5b0a49867940c62ba24d4f6a408ceae0d81cf35
Alexandru Badiu <andu@ctrlz.ro>
2013-09-09 13:57:23 +0300

    Cleanup

---
+++ src/com/voidberg/drupaldroid/SystemServices.java
@@ -0,0 +1,16 @@
+package com.voidberg.drupaldroid;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import org.json.JSONObject;
+
+public class SystemServices {
+    private ServicesClient client;
+
+    public SystemServices(ServicesClient c) {
+        client = c;
+    }
+
+    public void Connect(AsyncHttpResponseHandler responseHandler) {
+        client.post("system/connect", new JSONObject(), responseHandler);
+    }
+}
