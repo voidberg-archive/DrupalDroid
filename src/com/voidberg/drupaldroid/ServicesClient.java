02d8e53a8bf262edb4bf2c22a7d755969e07211f
Alexandru Badiu <andu@ctrlz.ro>
2013-09-09 15:02:20 +0300

    Added support for CSRF token.

--- src/com/voidberg/drupaldroid/ServicesClient.java
+++ src/com/voidberg/drupaldroid/ServicesClient.java
@@ -14,11 +14,30 @@ import java.io.*;
 
 public class ServicesClient {
     private String url;
+    private String rootUrl;
+    private String token;
+
     public static AsyncHttpClient client = new AsyncHttpClient();
 
     public ServicesClient(String server, String base) {
         this.url = server + '/' + base + '/';
+        this.rootUrl = server + '/';
+        this.token = "";
         client.setTimeout(60000);
+    }
+
+    public String getToken() {
+      return token;
+    }
+
+    public void setToken(String token) {
+      this.token = token;
+    }
+
+    private void setHeaders() {
+      if (!token.equals("")) {
+        client.addHeader("X-CSRF-Token", token);
+      }
     }
 
     public void setCookieStore(PersistentCookieStore cookieStore) {
@@ -29,15 +48,25 @@ public class ServicesClient {
         return this.url + relativeUrl;
     }
 
+    private String getAbsoluteRootUrl(String relativeUrl) {
+      return this.rootUrl + relativeUrl;
+    }
+
+    public void getRoot(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+      client.get(getAbsoluteRootUrl(url), params, responseHandler);
+    }
+
     public void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
     public void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        this.setHeaders();
         client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
     public void post(String url, JSONObject params, AsyncHttpResponseHandler responseHandler) {
+        this.setHeaders();
         StringEntity se = null;
         try {
             se = new StringEntity(params.toString());
@@ -50,10 +79,12 @@ public class ServicesClient {
     }
 
     public void put(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        this.setHeaders();
         client.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
     public void put(String url, JSONObject params, AsyncHttpResponseHandler responseHandler) {
+        this.setHeaders();
         StringEntity se = null;
         try {
             se = new StringEntity(params.toString());
@@ -63,5 +94,9 @@ public class ServicesClient {
         se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
 
         client.put(null, getAbsoluteUrl(url), se, "application/json", responseHandler);
+    }
+    
+    public void getToken(AsyncHttpResponseHandler responseHandler) {
+      this.getRoot("services/session/token", new RequestParams(), responseHandler);
     }
 }
